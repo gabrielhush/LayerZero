ddc91407f4fca85e969dcdae5d5cd957e9d47470
gabrielhush <151517538+gabrielhush@users.noreply.github.com>
2023-11-20 11:44:11 +0200

    Update approveSignerRelayer.js

--- tasks/approveSignerRelayer.js
+++ tasks/approveSignerRelayer.js
@@ -1,7 +1,7 @@
 module.exports = async function (taskArgs, hre, runSuper) {
     let relayer = await ethers.getContract("Relayer")
     if ((await relayer.isApproved(taskArgs.address)) && taskArgs.enable) {
-        console.log(`Relayer.isApproved(${taskArgs.address}) => true | *already approved*`)
+        console.log(`Relayer.isApproved(#{taskArgs.address}) => true | *already approved*`)
         return
     }
     let tx = await (await relayer.setApprovedAddress(taskArgs.address, taskArgs.enable)).wait()
